2066ade6f684c3e189e71806be508f2fccc3655c
Carlos <pcass78@gmail.com>
2018-08-12 13:14:42 -0600

    add r script

--- caso.R
+++ caso.R
@@ -5,6 +5,10 @@
 library(DataExplorer)
 library(readxl)
 library(dplyr)
+library(chron)
+library(lubridate)
+library(plyr)
+library(geosphere)
 
 # Import Data
 data <- read_excel("Downloads/Taxi-cancellation-case.xlsx")
@@ -19,13 +23,10 @@ plot_bar(data)
 # Columns with date values in unix format
 date_colums <- c("from_date", "to_date", "booking_created")
 
-# Formating values if each columns 
+# Formating values if each date columns to a readble one
 for (col in date_colums) {
   data[[col]] = as.POSIXct(as.Date(as.numeric(data[[col]]), origin="1899-12-30"))
 }
-
-# Missing Data plot
-plot_missing(data)
 
 # Change the values of Cancellation to Yes or No.
 data <- data %>% mutate(Car_Cancellation = 
@@ -46,7 +47,7 @@ data <- data %>% mutate(booking_type =
 
 factor_colums <- c("Car_Cancellation", "booking_type", "user_id", "vehicle_model_id",
                    "travel_type_id", "package_id", "from_area_id", "to_area_id",
-                   "from_city_id","to_city_id")
+                   "from_city_id","to_city_id", "period")
 
 # Changing type if each columns 
 for (col in factor_colums) {
@@ -57,12 +58,44 @@ for (col in factor_colums) {
 summary(data)
 
 # Create Dummy columns to day
-data$timestamp <- as.POSIXct(paste(data$booking_created, data$booking_created), format="%Y-%m-%d %H:%M:%S")
+data$period <- as.POSIXct(paste(data$booking_created, data$booking_created), format="%Y-%m-%d %H:%M:%S")
 
-data$intervals <- format(strptime(as.character(data$intervals), "%H:%M:%S"), "%H:%M:%S")
+data$period <- mapvalues(hour(data$period),from=c(0:23),
+          to=c(rep("Night",times=5), rep("Morning",times=6), 
+               rep("Afternoon",times=5),rep("Night", times=8)))
 
-format(strptime(as.character(data$booking_created), "%H:%M:%S"), "%H:%M:%S")
+data$booking_day <- weekdays(as.Date(data$booking_created)) # Create Days columns
+data$booking_month <- months(as.Date(data$booking_created)) # Create month columns
 
 
+# Create a new column with the distance of the travels
 
+data$to_long = as.numeric(data$to_long) # Convert to long to a numeric type
+data$to_lat = as.numeric(data$to_lat)   # Convert to lat to a numeric type
 
+plot_missing(data) # A lot of missing values  
+
+geo_colums <- c("from_lat", "from_long", "to_lat", "to_long")
+
+for (col in geo_colums) {
+  data[[col]] <- ifelse(is.na(data[[col]]), mean(data[[col]], na.rm=TRUE), 
+                        data[[col]])                                                      
+}
+
+f <- function(x){
+  a <- x["from_long"]
+  b <- x["from_lat"]
+  c <- x["to_long"]
+  d <- x["to_lat"]
+  print(c(a,b))
+  print(c(c,d))
+  distm( c(a,b), c(c,d), fun = distHaversine)
+}
+data$distance <- apply( data, 1,  f  )
+  
+# Missing Data plot
+plot_missing(data)  
+  
+  
+  
+  
