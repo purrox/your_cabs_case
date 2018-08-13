9679cbf6efe7edeac369de03901bb1341a5c41c7
Carlos <pcass78@gmail.com>
2018-08-13 13:17:56 -0600

    add r script

--- caso.R
+++ caso.R
@@ -9,6 +9,10 @@ library(chron)
 library(lubridate)
 library(plyr)
 library(geosphere)
+library(ggplot2)
+library(GGally)
+library(polycor)
+library(gridExtra)
 
 # Import Data
 data <- read_excel("Downloads/Taxi-cancellation-case.xlsx")
@@ -47,7 +51,7 @@ data <- data %>% mutate(booking_type =
 
 factor_colums <- c("Car_Cancellation", "booking_type", "user_id", "vehicle_model_id",
                    "travel_type_id", "package_id", "from_area_id", "to_area_id",
-                   "from_city_id","to_city_id", "period")
+                   "from_city_id","to_city_id", "period", "booking_day","booking_month")
 
 # Changing type if each columns 
 for (col in factor_colums) {
@@ -83,19 +87,45 @@ for (col in geo_colums) {
 }
 
 f <- function(x){
-  a <- x["from_long"]
-  b <- x["from_lat"]
-  c <- x["to_long"]
-  d <- x["to_lat"]
-  print(c(a,b))
-  print(c(c,d))
-  distm( c(a,b), c(c,d), fun = distHaversine)
+  a <- as.numeric(x["from_long"])
+  b <- as.numeric(x["from_lat"])
+  c <- as.numeric(x["to_long"])
+  d <- as.numeric(x["to_lat"])
+  distm(c(a, b), c(c, d), fun = distHaversine)
 }
+
 data$distance <- apply( data, 1,  f  )
   
 # Missing Data plot
 plot_missing(data)  
   
-  
-  
-  
+#Calculating waiting time
+data$waitingTime = difftime(data$from_date,data$booking_created, units = "mins")  
+
+# Graphing some interesting statistics
+
+# Relation of each cancellation with vehicles
+vehicle_table <- table(data$Car_Cancellation , data$vehicle_model_id)  
+
+barplot(vehicle_table, main="Cancellation Distribution for Vehicle",
+        xlab="Vehicle", col=c("darkblue","red"),
+        legend = rownames(vehicle_table))
+
+grid.table(vehicle_table)
+
+# Days of the week and cancellations
+
+data.cancellation <- data[data$Car_Cancellation == 'Yes', ]
+
+cancellation <- table(data.cancellation$Car_Cancellation, data.cancellation$booking_day)  
+
+print(cancellation) 
+
+barplot(cancellation, main="Cancellation Distribution for Vehicle",
+        xlab="Vehicle", col=c("darkblue","red"),
+        legend = rownames(cancellation),beside=TRUE )
+
+plot(data$booking_day, data$Car_Cancellation)
+
+
+
